900f4e5f8f51c53f078ceb478b8328b55457905e
quangduymanh <quangduymanh4@gmail.com>
2025-03-06 08:18:18 +0700

    upload bài 01

---
+++ seesion 21-bài 01.js
@@ -0,0 +1,8 @@
+let sumOdd = 0;
+for (let i = 1; i <= 5; i++) {
+    let num = +prompt(`Nhập số nguyên thứ ${i}:`);
+    if (num % 2 !== 0) { 
+        sumOdd += num; 
+    }
+}
+document.write(`Tổng các số lẻ là: ${sumOdd}`);
